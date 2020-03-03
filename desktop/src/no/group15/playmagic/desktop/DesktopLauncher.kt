83103a7a5e104349dd358a2cb0513e763a04332e
DeimosA <krvolden@gmail.com>
2020-03-03 12:44:14 +0100

    convert desktoplauncher

---
+++ desktop/src/no/group15/playmagic/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package no.group15.playmagic.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import no.group15.playmagic.PlayMagic
+
+object DesktopLauncher {
+	@JvmStatic
+	fun main(arg: Array<String>) {
+		val config = LwjglApplicationConfiguration()
+		LwjglApplication(PlayMagic(), config)
+	}
+}
