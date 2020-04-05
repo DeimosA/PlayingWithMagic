4236a12c9139d6105a2a0c883fee12894cd4b41e
Jacopo <jacopo.gargiulo@icloud.com>
2020-04-05 11:18:18 +0200

    add HealthSystem skeleton

---
+++ core/src/no/group15/playmagic/ecs/components/HealthComponent.kt
@@ -0,0 +1,7 @@
+package no.group15.playmagic.ecs.components
+
+import com.badlogic.ashley.core.Component
+
+class HealthComponent : Component {
+	var points : Int = 100 // TODO chose a good default value?
+}
---
+++ core/src/no/group15/playmagic/events/ExplosionHitsPlayerEvent.kt
@@ -0,0 +1,7 @@
+package no.group15.playmagic.events
+
+import com.badlogic.ashley.core.Entity
+
+class ExplosionHitsPlayerEvent(
+	val player: Entity
+)
