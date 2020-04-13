c07acf94eaf650cf1c9e101917a02fda8155d479
Jacopo <jacopo.gargiulo@icloud.com>
2020-04-13 13:01:49 +0200

    add game map logic

--- core/src/no/group15/playmagic/ecs/Factories.kt
+++ core/src/no/group15/playmagic/ecs/Factories.kt
@@ -10,6 +10,7 @@ import com.badlogic.gdx.utils.viewport.Viewport
 import no.group15.playmagic.ecs.components.MovementComponent
 import no.group15.playmagic.ecs.components.TextureComponent
 import no.group15.playmagic.ecs.components.TransformComponent
+import no.group15.playmagic.GameMap
 import no.group15.playmagic.ecs.systems.MovementSystem
 import no.group15.playmagic.ecs.systems.RenderingSystem
 import no.group15.playmagic.utils.assets.GameAssets
@@ -30,6 +31,8 @@ fun engineFactory(viewport: Viewport, batch: SpriteBatch, assetManager: AssetMan
 	entity.add(texture)
 	entity.add(engine.createComponent(MovementComponent::class.java))
 	engine.addEntity(entity)
+
+	GameMap(assetManager).makeEntities(engine)
 
 	// Add systems
 	engine.addSystem(MovementSystem(0, viewport))
