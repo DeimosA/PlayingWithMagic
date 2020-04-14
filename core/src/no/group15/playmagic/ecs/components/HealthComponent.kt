a0230b42066d7c4ef937a7d1b9728528f7348953
Jacopo <jacopo.gargiulo@icloud.com>
2020-04-14 11:24:53 +0200

    healthComponent: add maxPoints, add interface Poolable

--- core/src/no/group15/playmagic/ecs/components/HealthComponent.kt
+++ core/src/no/group15/playmagic/ecs/components/HealthComponent.kt
@@ -1,7 +1,14 @@
 package no.group15.playmagic.ecs.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
 
-class HealthComponent : Component {
-	var points : Int = 100 // TODO chose a good default value?
+class HealthComponent: Component, Pool.Poolable {
+	var points: Int = 100 // TODO choose a good default value?
+	var maxPoints: Int = 100
+
+	override fun reset() {
+		points = 100
+		maxPoints = 100
+	}
 }
