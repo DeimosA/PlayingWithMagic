55afe4364a330745b352a7aeb84dbf66b1b50a27
skykanin <3789764+skykanin@users.noreply.github.com>
2020-04-17 13:11:44 +0200

    Fix bug in command function where it returns unit vector on bad input and make step sane

--- core/src/no/group15/playmagic/ecs/Command.kt
+++ core/src/no/group15/playmagic/ecs/Command.kt
@@ -8,10 +8,10 @@ import com.badlogic.gdx.Input
  * Commands which the input events are mapped to for the different input events
  */
 
-fun move(dir: Int, pos: ImmutableVector2, step: Int = 10) = when (dir) {
+fun move(dir: Int, pos: ImmutableVector2, step: Float = 0.2f) = when (dir) {
 	Input.Keys.UP -> pos.copy(y = pos.y + step)
 	Input.Keys.DOWN -> pos.copy(y = pos.y - step)
 	Input.Keys.LEFT -> pos.copy(pos.x - step)
 	Input.Keys.RIGHT -> pos.copy(pos.x + step)
-	else -> ImmutableVector2(0f, 0f)
+	else -> pos
 }
