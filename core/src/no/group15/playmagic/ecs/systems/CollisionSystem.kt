21f1c56abb0972695d8545b297f217a6688b2ae7
Jacopo <jacopo.gargiulo@icloud.com>
2020-04-20 11:47:57 +0200

    remove entityPairs() method

--- core/src/no/group15/playmagic/ecs/systems/CollisionSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/CollisionSystem.kt
@@ -1,12 +1,11 @@
 package no.group15.playmagic.ecs.systems
 
-import com.badlogic.ashley.core.Engine
-import com.badlogic.ashley.core.Entity
-import com.badlogic.ashley.core.EntitySystem
-import com.badlogic.ashley.core.Family
+import com.badlogic.ashley.core.*
 import com.badlogic.ashley.signals.Listener
 import com.badlogic.ashley.signals.Signal
 import com.badlogic.ashley.utils.ImmutableArray
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.*
 import ktx.ashley.allOf
 import ktx.ashley.get
@@ -43,25 +42,28 @@ class CollisionSystem(
 
 	override fun update(deltaTime: Float) {
 
-		for ( (entity1, entity2) in entityPairs() ) {
+		for ( i in 0 until entities.size()) {
+			for (j in i + 1 until entities.size()) {
+				var entity1 = entities[i]
+				var entity2 = entities[j]
 
-			if (areColliding(entity1, entity2)) {
+				if (areColliding(entity1, entity2)) {
 
-				if (isNewCollision(entity1, entity2)) {
-					selectiveDispatch(CollisionEvent(entity1, entity2))
+					if (isNewCollision(entity1, entity2)) {
+						selectiveDispatch(CollisionEvent(entity1, entity2))
+					}
+
+					// component state update
+					entity1[collision]!!.collidingWith.add(entity2)
+					entity2[collision]!!.collidingWith.add(entity1)
+
+				} else {
+					// component state update
+					entity1[collision]!!.collidingWith.remove(entity2)
+					entity2[collision]!!.collidingWith.remove(entity1)
 				}
 
-				// component state update
-				entity1[collision]!!.collidingWith.add(entity2)
-				entity2[collision]!!.collidingWith.add(entity1)
-
 			}
-			else {
-				// component state update
-				entity1[collision]!!.collidingWith.remove(entity2)
-				entity2[collision]!!.collidingWith.remove(entity1)
-			}
-
 		}
 
 	}
@@ -137,20 +139,6 @@ class CollisionSystem(
 				signal.dispatch(event)
 			}
 		}
-	}
-
-
-
-	private fun entityPairs(): Iterable<Pair<Entity, Entity>> {
-		val pairs: MutableList<Pair<Entity, Entity>> = LinkedList()
-
-		for ( i in 0 until entities.size()) {
-			for ( j in i+1 until entities.size()) {
-				pairs.add( Pair(entities[i], entities[j]) )
-			}
-		}
-
-		return pairs
 	}
 
 
