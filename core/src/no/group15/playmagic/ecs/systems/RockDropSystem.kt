80be040ddec4a0cbb1dd82fd7140fa61f1e58108
alek94s <alek_straumsheim@hotmail.com>
2020-04-23 05:02:41 +0200

    Rockdestructionevent

--- core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
@@ -1,43 +1,40 @@
 package no.group15.playmagic.ecs.systems
 
-import com.badlogic.ashley.core.Engine
-import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
-import com.badlogic.ashley.core.Family
-
-import com.badlogic.ashley.utils.ImmutableArray
+import com.badlogic.ashley.signals.Listener
+import com.badlogic.ashley.signals.Signal
+import ktx.ashley.has
+import ktx.ashley.mapperFor
+import ktx.ashley.remove
 import no.group15.playmagic.ecs.components.DestructibleComponent
-import no.group15.playmagic.ecs.components.TextureComponent
-import no.group15.playmagic.ecs.components.TransformComponent
+import no.group15.playmagic.ecs.engineFactory
 
+import no.group15.playmagic.events.RockDestructionEvent
+
+// unsure if this is the right way to do this
 class RockDropSystem (
 	priority: Int
 	) : EntitySystem(
 	priority
-	){
+	), Listener<RockDestructionEvent> {
 
-	private lateinit var destructibleRocks: ImmutableArray<Entity>
+	private val destructible = mapperFor<DestructibleComponent>()
 
-	override fun addedToEngine ( engine : Engine) {
-		destructibleRocks = engine.getEntitiesFor(
-			Family.all(DestructibleComponent::class.java, TransformComponent::class.java,
-				TextureComponent::class.java).get()
-		)
+	override fun receive(signal: Signal<RockDestructionEvent>,
+						 event: RockDestructionEvent) {
+		assert( event.rock.has(destructible))
+
+		engine.removeEntity(event.rock) //
+
+		// TODO: random chance to create pickup
+
+
+
 	}
 
-	override fun update(deltaTime: Float) {
-		for (r in destructibleRocks) {
-			var rock = r
-			// TODO(): check for rockdestructionevent,
-			//if rockdestructionevent:
-			engine.removeEntity(rock)
-			//just a random drop chance
-			if ((0..9).random() > 4) {
-				// TODO(): create powerup entity
-			}
+
 		}
-	}
-}
+
 
 
 
