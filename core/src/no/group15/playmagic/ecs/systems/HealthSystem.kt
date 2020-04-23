9285dbc81d606f813862193ae8bfa4476a93bfdd
Jacopo <jacopo.gargiulo@icloud.com>
2020-04-23 16:54:49 +0200

    change event in HealtSystem

--- core/src/no/group15/playmagic/ecs/systems/HealthSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/HealthSystem.kt
@@ -6,7 +6,10 @@ import com.badlogic.ashley.signals.Signal
 import ktx.ashley.get
 import ktx.ashley.has
 import ktx.ashley.mapperFor
+import no.group15.playmagic.ecs.components.ExploderComponent
 import no.group15.playmagic.ecs.components.HealthComponent
+import no.group15.playmagic.ecs.components.TransformComponent
+import no.group15.playmagic.events.CollisionEvent
 import no.group15.playmagic.events.ExplosionHitsPlayerEvent
 
 
@@ -16,20 +19,30 @@ class HealthSystem(
 	priority: Int
 ) : EntitySystem(
 	priority
-), Listener<ExplosionHitsPlayerEvent> {
+), Listener<CollisionEvent> {
 
 	private val health = mapperFor<HealthComponent>()
+	private val exploder = mapperFor<ExploderComponent>()
+	private val transform = mapperFor<TransformComponent>()
 
 
-	override fun receive(signal: Signal<ExplosionHitsPlayerEvent>,
-						 event: ExplosionHitsPlayerEvent) {
+	override fun receive(signal: Signal<CollisionEvent>,
+						 event: CollisionEvent) {
 
-		assert( event.player.has(health) ) { "The entity hasn't a Health Component." }
+		val bomb = if (event.entity1.has(exploder)) event.entity1 else event.entity2
+		val player = if (event.entity1.has(health)) event.entity1 else event.entity2
+
+		player[transform]!!.position.set(0f, 0f)
+		player[transform]!!.boundingBox.setCenter(0f, 0f)
+		//assert( event.player.has(health) ) { "The entity hasn't a Health Component." }
 
 		// !! required because player[health] type is nullable
 		// ? gives error with -= operator
-		event.player[health]!!.points -= BOMB_EXPLOSION_DAMAGE
+		//event.player[health]!!.points -= BOMB_EXPLOSION_DAMAGE
 
 	}
+
+
+
 
 }
