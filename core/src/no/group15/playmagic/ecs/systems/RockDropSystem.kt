75badb957baa5e16bf1e5c2a4f9a45c38c37bf7b
alek94s <alek_straumsheim@hotmail.com>
2020-04-23 06:20:37 +0200

    Changes to RockDropSystem

--- core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
@@ -1,43 +1,30 @@
 package no.group15.playmagic.ecs.systems
 
-import com.badlogic.ashley.core.Engine
-import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
-import com.badlogic.ashley.core.Family
-
-import com.badlogic.ashley.utils.ImmutableArray
+import com.badlogic.ashley.signals.Listener
+import com.badlogic.ashley.signals.Signal
+import ktx.ashley.has
+import ktx.ashley.mapperFor
 import no.group15.playmagic.ecs.components.DestructibleComponent
-import no.group15.playmagic.ecs.components.TextureComponent
-import no.group15.playmagic.ecs.components.TransformComponent
 
+import no.group15.playmagic.events.RockDestructionEvent
+
+// unsure if this is the right way to do this
 class RockDropSystem (
 	priority: Int
-	) : EntitySystem(
+) : EntitySystem(
 	priority
-	){
+), Listener<RockDestructionEvent> {
 
-	private lateinit var destructibleRocks: ImmutableArray<Entity>
+	private val destructible = mapperFor<DestructibleComponent>()
 
-	override fun addedToEngine ( engine : Engine) {
-		destructibleRocks = engine.getEntitiesFor(
-			Family.all(DestructibleComponent::class.java, TransformComponent::class.java,
-				TextureComponent::class.java).get()
-		)
-	}
+	override fun receive(signal: Signal<RockDestructionEvent>,
+						 event: RockDestructionEvent) {
+		assert( event.rock.has(destructible))
 
-	override fun update(deltaTime: Float) {
-		for (r in destructibleRocks) {
-			var rock = r
-			// TODO(): check for rockdestructionevent,
-			//if rockdestructionevent:
-			engine.removeEntity(rock)
-			//just a random drop chance
-			if ((0..9).random() > 4) {
-				// TODO(): create powerup entity
-			}
+		engine.removeEntity(event.rock)
+		if((0 .. 9).random() > 4) { // just a random chance
+			// TODO:  create pickup with same position as the rock
 		}
 	}
 }
-
-
-
