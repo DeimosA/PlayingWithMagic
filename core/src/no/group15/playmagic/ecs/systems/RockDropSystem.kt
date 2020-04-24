4a3156a0112a32b9cd736eb7931f59649294d37d
alek94s <alek_straumsheim@hotmail.com>
2020-04-24 03:54:21 +0200

    chance to drop pickup when rock destroyed

--- core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
@@ -1,30 +1,46 @@
 package no.group15.playmagic.ecs.systems
 
 import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.ashley.signals.Listener
 import com.badlogic.ashley.signals.Signal
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.math.Vector2
 import ktx.ashley.has
 import ktx.ashley.mapperFor
 import no.group15.playmagic.ecs.components.DestructibleComponent
+import no.group15.playmagic.events.CollisionEvent
+import ktx.ashley.*
+import no.group15.playmagic.ecs.components.TransformComponent
+import no.group15.playmagic.ecs.entities.EntityFactory
 
-import no.group15.playmagic.events.RockDestructionEvent
 
 // unsure if this is the right way to do this
 class RockDropSystem (
-	priority: Int
+	priority: Int,
+	private val assetManager: AssetManager
 ) : EntitySystem(
 	priority
-), Listener<RockDestructionEvent> {
-
+), Listener<CollisionEvent> {
 	private val destructible = mapperFor<DestructibleComponent>()
+	private val transform = mapperFor<TransformComponent>()
 
-	override fun receive(signal: Signal<RockDestructionEvent>,
-						 event: RockDestructionEvent) {
-		assert( event.rock.has(destructible))
+	override fun receive(signal: Signal<CollisionEvent>,
+						 event: CollisionEvent) {
 
-		engine.removeEntity(event.rock)
-		if((0 .. 9).random() > 4) { // just a random chance
-			// TODO:  create pickup with same position as the rock
-		}
+
+		val rock = if(event.entity1.has(destructible))event.entity1
+		else event.entity2
+		val rockPos = rock[transform]!!.position
+		engine.removeEntity(rock)
+	//	if((0 .. 9).random() > 4) { // just a random chance
+			var pickUp = EntityFactory.makeEntity(assetManager, engine as PooledEngine, EntityFactory.Type.ROCK)
+			pickUp[transform]!!.position.set(rockPos.x, rockPos.y)
+			pickUp[transform]!!.boundingBox.setCenter(pickUp[transform]!!.position)
+
+	//	}
 	}
+
+
 }
+
--- core/src/no/group15/playmagic/events/RockDestructionEvent.kt
+++
@@ -1,7 +0,0 @@
-package no.group15.playmagic.events
-
-import com.badlogic.ashley.core.Entity
-
-class RockDestructionEvent (
-	val rock: Entity
-)
