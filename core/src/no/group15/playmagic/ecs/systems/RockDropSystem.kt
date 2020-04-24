d9fed94b22a6cbafcea51cde525d1bc39aeaa6d2
alek94s <alek_straumsheim@hotmail.com>
2020-04-24 05:35:55 +0200

    chance to drop pickup when rock destroyed

--- core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
@@ -1,5 +1,6 @@
 package no.group15.playmagic.ecs.systems
 
+import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.ashley.signals.Listener
@@ -11,6 +12,7 @@ import ktx.ashley.mapperFor
 import no.group15.playmagic.ecs.components.DestructibleComponent
 import no.group15.playmagic.events.CollisionEvent
 import ktx.ashley.*
+import no.group15.playmagic.ecs.components.ExploderComponent
 import no.group15.playmagic.ecs.components.TransformComponent
 import no.group15.playmagic.ecs.entities.EntityFactory
 
@@ -24,21 +26,26 @@ class RockDropSystem (
 ), Listener<CollisionEvent> {
 	private val destructible = mapperFor<DestructibleComponent>()
 	private val transform = mapperFor<TransformComponent>()
+	private val exploder = mapperFor<ExploderComponent>()
+
 
 	override fun receive(signal: Signal<CollisionEvent>,
 						 event: CollisionEvent) {
 
+		val bomb = if (event.entity1.has(exploder)) event.entity1 else event.entity2
+		val destructible = if(event.entity1.has(destructible))event.entity1 else event.entity2
+		if(bomb[exploder]!!.isExploded) {
+			engine.removeEntity(destructible)
 
-		val rock = if(event.entity1.has(destructible))event.entity1
-		else event.entity2
-		val rockPos = rock[transform]!!.position
-		engine.removeEntity(rock)
-	//	if((0 .. 9).random() > 4) { // just a random chance
-			var pickUp = EntityFactory.makeEntity(assetManager, engine as PooledEngine, EntityFactory.Type.ROCK)
-			pickUp[transform]!!.position.set(rockPos.x, rockPos.y)
-			pickUp[transform]!!.boundingBox.setCenter(pickUp[transform]!!.position)
 
-	//	}
+		}
+		val rockPos = destructible[transform]!!.position
+			if((0 .. 9).random() > 4) {  //just a random chance
+		var pickUp = EntityFactory.makeEntity(assetManager, engine as PooledEngine, EntityFactory.Type.PICKUP)
+		pickUp[transform]!!.position.set(rockPos.x, rockPos.y)
+		pickUp[transform]!!.boundingBox.setCenter(pickUp[transform]!!.position)
+
+		//	}
 	}
 
 
