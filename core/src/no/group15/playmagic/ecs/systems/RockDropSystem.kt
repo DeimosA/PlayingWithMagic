86a2dd8b04281256cec652b697dcc57aed63635b
alek94s <alek_straumsheim@hotmail.com>
2020-04-24 05:39:28 +0200

    chance to drop pickup when rock destroyed

--- core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
+++ core/src/no/group15/playmagic/ecs/systems/RockDropSystem.kt
@@ -31,11 +31,11 @@ class RockDropSystem (
 
 		val bomb = if (event.entity1.has(exploder)) event.entity1 else event.entity2
 		val destructible = if(event.entity1.has(destructible))event.entity1 else event.entity2
-		if(bomb[exploder]!!.isExploded) {
+		//if(bomb[exploder]!!.isExploded) {
 			engine.removeEntity(destructible)
 
 
-		}
+		//}
 		val rockPos = destructible[transform]!!.position
 			if((0 .. 9).random() > 4) {  //just a random chance
 		var pickUp = EntityFactory.makeEntity(assetManager, engine as PooledEngine, EntityFactory.Type.PICKUP)
